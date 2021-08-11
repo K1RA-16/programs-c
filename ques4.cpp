55fa1e37313bcb06e7c3af744159603ad374249a
nij mehar <nijmehar@Gurus-MacBook-Air.local>
2021-08-11 18:05:29 +0530

    ques4/450

--- ques4.cpp
+++ ques4.cpp
@@ -5,7 +5,18 @@ int main(){
     cout<<"enter a three index array"<<endl;
     for(int i=0;i<3;i++){
         cin>>arr[i];
+    }  
+    for(int i=0;i<2;i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            int k = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = k;
+        }
     }
-    
+     for(int i=0;i<3;i++){
+        cout<<arr[i]<<" ";
+    }  
     return 0;
 }

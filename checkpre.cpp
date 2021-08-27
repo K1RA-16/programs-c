240338a7298d738686b10cfc8ef874bcd3bbe916
Nij Mehar <K1RA-16@users.noreply.github.com>
2021-08-27 15:13:13 +0530

    tabs in checkpre.cpp

--- checkpre.cpp
+++ checkpre.cpp
@@ -3,12 +3,12 @@ using namespace std;
 #define num 10
 int main()
 {
-int s= num;
-int a[]={12,31,12};
-int size=sizeof(a);
+    int s= num;
+    int a[]={12,31,12};
+    int size=sizeof(a);
 
-cout<<s<<endl;
-return 0;
+    cout<<s<<endl;
+    return 0;
 
 }
 //does not work because of num being a preprocessor deined value
